49324723b895bbd41fca803b2563a5b0eb0e3801
Daniel Mera <daniel.mera@epn.edu.ec>
2023-11-29 10:20:19 -0500

    Creacion clases

---
+++ src/main/java/logica/Libro.java
@@ -0,0 +1,72 @@
+package logica;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Libro {
+
+    private String titulo, autor, genero, isbn;
+    private boolean disponibilidad;
+
+    public Libro() {
+    }
+
+    public Libro(String titulo, String autor, String genero, String isbn, boolean disponibilidad) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.genero = genero;
+        this.isbn = isbn;
+        this.disponibilidad = disponibilidad;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public boolean isDisponibilidad() {
+        return disponibilidad;
+    }
+
+    public void setDisponibilidad(boolean disponibilidad) {
+        this.disponibilidad = disponibilidad;
+    }
+
+    public static List<Libro> verificarLibrosDisponibles(List<Libro> listaLibros) {
+        List<Libro> librosValidos = new ArrayList<>();
+
+        for (Libro libro : listaLibros) {
+            if (libro.isDisponibilidad()) {
+                librosValidos.add(libro);
+            }
+        }
+        return librosValidos;
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,45 @@
-<html>
+<%@ page import="logica.*" %>
+<%@ page import="java.util.List" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/solicitarPrestamo.css">
+    <title>Solicitar Préstamo</title>
+</head>
+
 <body>
-<h2>Hello World!</h2>
+<div class="container">
+    <h1>Solicitar Préstamo</h1>
+    <p>Seleccione el estudiante registrado y los libros disponibles para realizar un préstamo.</p>
+
+    <div class="tables-container">
+        <table class="books-table">
+        </table>
+    </div>
+
+    <tr>
+        <th colspan="2">&nbsp;&nbsp;&nbsp;</th>
+        <th colspan="2">&nbsp;&nbsp;&nbsp;</th>
+        <th colspan="2">&nbsp;&nbsp;&nbsp;</th>
+    </tr>
+
+    <div class="formulario">
+        <form action="solicitarPrestamo" method="post">
+            <label for="idLibro">&nbsp;&nbsp;&nbsp;ID del logica.Libro:</label>
+            <input type="text" id="idLibro" name="idLibro" placeholder="Ingrese el ID aquí">
+
+            <button type="submit">Aceptar</button>
+        </form>
+    </div>
+
+    <table class="loans-table">
+    </table>
+
+</div>
 </body>
 </html>
+
